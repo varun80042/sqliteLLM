c0bdb1fcc6d28004e87f3a1bf39d2d0514beae3e
varun <varun80042@gmail.com>
2024-11-01 17:54:22 +0530

    fix db

--- helper/load_db.py
+++ helper/load_db.py
@@ -7,7 +7,7 @@ conn = sqlite3.connect(db_path)
 try:
     elections_2019_df = pd.read_csv("data/final/final_details_of_assembly_segment_2019.csv")
     elections_2024_df = pd.read_csv("data/final/final_eci_data_2024.csv")
-    maha_2019_df = pd.read_csv("data/final/maha_results_2019.csv")
+    maha_2019_df = pd.read_csv("data/final/final_maha_results_2019.csv")
 
     elections_2019_df.to_sql("elections_2019", conn, if_exists="replace", index=False)
     elections_2024_df.to_sql("elections_2024", conn, if_exists="replace", index=False)
